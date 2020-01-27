7b6d52856e8e50d4def3487b17ac4a84cf9cc7d7
Abdul Abdulbawab <aabdulbawab@horlingseneerbeek.nl>
2020-01-27 16:37:23 +0100

    m

---
+++ 02 Ej2.blazor_syncfusion-Form-EditForm/WebApplication1/Model/Notitie.cs
@@ -0,0 +1,30 @@
+﻿namespace HenE_Application.Model
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json.Serialization;
+
+    public class Notitie
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Notitie"/> class.
+        /// </summary>
+        /// <param name="id">Id.</param>
+        public Notitie(int id)
+        {
+            this.Id = id;
+        }
+
+        [Required]
+        [JsonConverter(typeof(Int32))]
+        public Int32 Id { get; private set; }
+
+        [Required]
+        public string Title { get; set; } = "r";
+
+        public string Body { get; set; } = "b";
+
+        [Required]
+        public DateTime Datum { get; set; }
+    }
+}
